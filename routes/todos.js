d0b80d58805382a759cb98bae8f3b0c2920a4100
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-11-14 15:29:06 +0530

    Establish a connection with db instance

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://dileep:dileep@ds037837.mlab.com:37837/todoapp',['todos']);
 
+// Get All Todos
 router.get('/todos', function(req, res, next){
-    res.send('TODOS API');
+    db.todos.find(function(err, todos){
+        if(err){
+           res.send(err); 
+        } else {
+            res.json(todos);
+        }
+    });
 });
+
 
 module.exports = router;
