d8c85da9f4fa39817f387fd21380d34880e59f7a
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-11-16 14:37:49 +0530

    added todo component

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {TodosComponent} from './components/todos.component';
 
 @Component({
-  moduleId: module.id,
+  moduleId:module.id,
   selector: 'my-app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  directives: [TodosComponent]
 })
 export class AppComponent { }
-
